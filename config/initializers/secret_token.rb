6998751180b82c5ba84d19718f886d441b61afdd
Marco <marco.a.cerna@gmail.com>
2014-02-15 10:53:38 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sonicnote::Application.config.secret_token = 'd9f2df841fe3f84c6aef82c70bb4304d718fc38900248fb2b85ad1edee83dfc04f56e3ed8de0c54dd0a003108a75a18c932667272642c59be29fdfad3216d413'
